203566b46607c0d5505a4f2dbb3e6a86b49aa37b
WandaCeV78 <60760292+WandaCeV78@users.noreply.github.com>
2020-02-21 22:03:04 -0300

    Add files via upload
    10

---
+++ jsIteraciones (10).js
@@ -0,0 +1,36 @@
+function mostrar()
+{
+	//declarar contadores y variables 
+	var acumulapos = 0;
+	var acumulaneg = 0;
+	var contpos = 0;
+	var contneg = 0;
+	var cantcero= 0;
+	var cantpar= 0;
+	var prompos= 0
+	var promneg = 0;
+	var dif= 0;
+
+
+	
+	var respuesta="si";
+
+	while(respuesta!="no")
+	{
+		
+	
+	}
+
+
+
+	document.write("La suma de los negativos: "+ acumulaneg + "</br>");
+	document.write("La suma de los positivos: "+ acumulapos + "</br>"); 
+	/*document.writeCantidad de positivos. 
+	document.writeCantidad de negativos. 
+	document.writeCantidad de ceros. 
+	document.writeCantidad de números pares. 
+	document.writePromedio de positivos. 
+	document.writePromedios de negativos. 
+	document.writeDiferencia entre positivos y negativos, (positvos-negativos)*/
+
+}//FIN DE LA FUNCIÓN
