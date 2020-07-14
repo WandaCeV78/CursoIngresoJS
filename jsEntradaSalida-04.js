e9a2b4da3b3f00b22e41cea9f10190a287d052ab
WandaCeV78 <60760292+WandaCeV78@users.noreply.github.com>
2020-07-13 22:21:27 -0300

    Add files via upload

---
+++ jsEntradaSalida-04.js
@@ -0,0 +1,14 @@
+/* 
+Debemos lograr tomar un dato por 'PROMPT' 
+y lo muestro por 'ID' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+	var nombre;
+
+	nombre = prompt( "Ingrese su nombre");
+
+	txtIdNombre.value= nombre; 
+	
+}
+
---
+++ jsEntradaSalida-06.js
@@ -0,0 +1,33 @@
+/*
+Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{
+
+var num1;
+var num2;
+var suma;
+
+
+num1 =txtIdNumeroUno.value;
+num2= txtIdNumeroDos.value;
+
+num1= parseInt(num1);
+num2= parseInt(num2);
+
+suma = num1 + num2;
+
+alert (suma);
+
+
+
+
+
+
+
+}
+
+
+
+
